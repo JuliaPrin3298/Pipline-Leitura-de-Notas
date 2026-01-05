dbe7058aa6190e9b1f8df0dbd4281b650b1c29b0
JuliaPrin3298 <juliaprinnena1123@gmail.com>
2026-01-04 21:54:21 -0300

    Criando o looping de Lista dos alunos

--- index.py
+++ index.py
@@ -15,8 +15,29 @@ df = df.dropna(subset=['Nomes'])
 #Seleção das colunas
 df_final = df[['Nomes', 'MédiaF']]
 
-# Mostra o resultado
-print(df_final.head(10))
+#Seleção das colunas
+df_final = df[['Nomes', 'MédiaF']].copy() # O .copy() evita avisos do Pandas
+
+# DICA DE SEGURANÇA: Converte a coluna para números, forçando erro se houver texto
+df_final['MédiaF'] = pd.to_numeric(df_final['MédiaF'], errors='coerce')
+
+#Criando a lista das notas
+lista_de_notas = df_final.values.tolist()
+
+#Mostra se o aluno foi reprovado ou aprovado
+print("\n=== Lista de Alunos ===")
+for aluno in lista_de_notas:
+    nome = aluno[0]
+    nota = aluno[1]
+   # Formatando para mostrar bonitinho
+    print(f"Aluno: {nome:<40} | Média: {nota:.1f}", end=' | ')
+    if(nota >= 7):
+        print("Aluno Aprovado")
+    else:
+        print("Aluno Reprovado")
+    
+# Mostra o resultado dos 10 primeiros alunos
+#print(df_final.head(10))
 
 # Lista completa, use:
 # print(df_final)
